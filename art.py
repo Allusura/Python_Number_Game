43e39bdcb3144f079043566a05df62cfcbcd0662
Allusura <110790823+Allusura@users.noreply.github.com>
2023-11-24 19:32:42 +0000

    Create art.py

---
+++ art.py
@@ -0,0 +1,22 @@
+# Created by D. Hopwood
+# Obtained from https://ascii.co.uk/
+logo = """
+             ________
+        _jgN########Ngg_
+      _N##N@@""  ""9NN##Np_
+     d###P            N####p
+     "^^"              T####
+                       d###P
+                    _g###@F
+                 _gN##@P
+               gN###F"
+              d###F
+             0###F
+             0###F
+             0###F
+             "NN@'
+
+              ___
+             q###r
+              ""
+"""
